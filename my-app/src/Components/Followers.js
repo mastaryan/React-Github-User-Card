0db11c2df34527c446e17e9683ee4ede0d4a94fd
mastaryan <sizemore.ryan@gmail.com>
2020-02-11 17:00:01 -0500

    Added axios and component files. Basic file structure

---
+++ my-app/src/Components/Followers.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../src/App.css';
+
+const FollowersCard = props => {
+    return(
+        <div className="followersCard">
+            {/* {props.map(follower =>{
+                return(
+                    <img src={follower.avatar_url} alt="follower"/>
+                )
+            })} */}
+        </div>
+        )}
+export default FollowersCard;
---
+++ my-app/src/Components/UserCard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const UserCard = props => {
+    return(
+        <div className="userCard">
+            <h1>username: {props.username}</h1>   
+        </div>
+
+        )}
+export default UserCard;
